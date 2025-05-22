7b0615d93f760dd4eb523f38f1eb84cc78cbffe6
venkatesh <venkateshvenky626@gmail.com>
2025-05-22 23:01:56 +0530

    first commit

---
+++ terraform/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "venky-state-dev"
+key    = "catalogue"
+region = "us-east-1"
+dynamodb_table = "venky-locking-dev"
---
+++ terraform/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "venky-state-prod"
+key    = "catalogue"
+region = "us-east-1"
+dynamodb_table = "venky-locking-prod"
